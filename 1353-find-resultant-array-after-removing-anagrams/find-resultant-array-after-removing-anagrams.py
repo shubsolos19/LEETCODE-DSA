573363085c8acbc12ea0698981260146312e1318
Shubham Bawari <shubhambawari123@gmail.com>
2025-10-13 22:03:23 +0530

    Time: 6 ms (53.93%) | Memory: 18 MB (28.68%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeAnagrams(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        def isAna(s1, s2):
+            return sorted(s1) == sorted(s2)
+        
+        r = []
+        for i in words:
+            if r and isAna(r[-1], i):
+                continue
+            r.append(i)
+        return r
