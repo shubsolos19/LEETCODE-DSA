73add7312bf27c4871315f6e6130e115fb0eb8c2
Shubham Bawari <shubhambawari123@gmail.com>
2026-01-26 01:00:38 +0530

    Time: 7 ms (79.53%) | Memory: 58.1 MB (24.56%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -0,0 +1,8 @@
+var minimumDifference = function(nums, k) {
+    nums.sort((a,b) => a - b)
+    let ans = Infinity, j = k - 1
+    for(let i = 0; i + j < nums.length; i++) {
+        ans = Math.min(ans, nums[i + j] - nums[i])
+    }
+    return ans
+};
